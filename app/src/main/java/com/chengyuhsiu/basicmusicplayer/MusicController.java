3851457beca937adddfa9703ef10fc9583f5ad33
chengyuhsiu <chengyuhsiu@gmail.com>
2015-01-11 10:27:36 +0800

    init

---
+++ app/src/main/java/com/chengyuhsiu/basicmusicplayer/MusicController.java
@@ -0,0 +1,13 @@
+package com.chengyuhsiu.basicmusicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by chengyuhsiu on 10/11/14.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context context) {
+        super(context);
+    }
+}
---
+++ app/src/main/java/com/chengyuhsiu/basicmusicplayer/Song.java
@@ -0,0 +1,20 @@
+package com.chengyuhsiu.basicmusicplayer;
+
+/**
+ * Created by chengyuhsiu on 10/11/14.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id = songID;
+        title = songTitle;
+        artist = songArtist;
+    }
+
+    public long getID() { return id; }
+    public String getTitle() { return title; }
+    public String getArtist() { return artist; }
+}
